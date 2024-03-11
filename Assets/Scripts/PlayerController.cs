5234b6424d1c88394ff3eb0195e8796de447c5aa
Roombie <120424080+Roombie@users.noreply.github.com>
2024-03-11 20:01:52 -0300

    Passing Unity project to GitHub

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool CanMove { get; private set; } = true;
+
+    [Header("Functional Options")]
+    [SerializeField] private bool canSprint = true;
+    [SerializeField] private bool canJump = true;
+    [SerializeField] private bool canCrouch = true;
+
+    [Header("Movement Parameters")]
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float sprintSpeed = 10f;
+    [SerializeField] private float crouchSpeed = 1.5f;
+
+    [Header("Jumping Parameters")]
+    [SerializeField] private float jumpForce = 8f;
+    [SerializeField] private float gravity = 30f;
+
+    [Header("Looking Parameters")]
+    [SerializeField] private float lookY;
+
+    [Header("Components")]
+    private Rigidbody rb;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
